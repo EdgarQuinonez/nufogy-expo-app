92731daeff7dc4692683b79dfb035c47911526c8
EdgarGlowbo <efqr811@gmail.com>
2024-06-14 16:44:18 -0600

    Imported hooks useAsync and useFetch into utils. Need to convert them to typescript

--- components/AddIngredientFormModal.tsx
+++ components/AddIngredientFormModal.tsx
@@ -6,6 +6,7 @@ import { Button, H1, View, XStack, YStack } from "tamagui";
 import SearchBar from "@components/SearchBar";
 import FoodSearchItem from "@components/FoodSearchItem";
 import { FoodSearchItemType } from "@types";
+import useFetch from "@utils/useFetch";
 
 export type Props = {
   visible: boolean;
@@ -17,34 +18,36 @@ export default function AddIngredientFormModal({
   onRequestClose,
 }: Props) {
   const [searchQuery, setSearchQuery] = useState("");
+  // const apiEndpoint = `${process.env.EXPO_PUBLIC_API_BASE_URL}/diary/fs/searchList/?query=${searchQuery}`;
+  const apiEndpoint = `https://nufogy-api.fly.dev//diary/fs/searchList/?query=${searchQuery}`;
 
-  const foods: FoodSearchItemType[] = [
-    {
-      food_id: 1,
-      food_name: "Apple",
-      brand_name: "Fruit Co.",
-      food_type: "Fruit",
-      food_url: "https://example.com/apple",
-      food_description: "A delicious and healthy fruit.",
-    },
-    {
-      food_id: 2,
-      food_name: "Banana",
-      brand_name: "Fruit Co.",
-      food_type: "Fruit",
-      food_url: "https://example.com/banana",
-      food_description: "A popular tropical fruit.",
-    },
-    {
-      food_id: 3,
-      food_name: "Carrot",
-      brand_name: "Vegetable Co.",
-      food_type: "Vegetable",
-      food_url: "https://example.com/carrot",
-      food_description: "A crunchy and nutritious vegetable.",
-    },
-  ];
-
+  // const foods: FoodSearchItemType[] = [
+  //   {
+  //     food_id: 1,
+  //     food_name: "Apple",
+  //     brand_name: "Fruit Co.",
+  //     food_type: "Fruit",
+  //     food_url: "https://example.com/apple",
+  //     food_description: "A delicious and healthy fruit.",
+  //   },
+  //   {
+  //     food_id: 2,
+  //     food_name: "Banana",
+  //     brand_name: "Fruit Co.",
+  //     food_type: "Fruit",
+  //     food_url: "https://example.com/banana",
+  //     food_description: "A popular tropical fruit.",
+  //   },
+  //   {
+  //     food_id: 3,
+  //     food_name: "Carrot",
+  //     brand_name: "Vegetable Co.",
+  //     food_type: "Vegetable",
+  //     food_url: "https://example.com/carrot",
+  //     food_description: "A crunchy and nutritious vegetable.",
+  //   },
+  // ];
+  const { value: foods } = useFetch(apiEndpoint, {}, [searchQuery]);
   useEffect(() => {
     if (visible) {
       StatusBar.setBarStyle("dark-content");
